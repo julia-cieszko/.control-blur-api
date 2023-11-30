c2baf124760fbc565903779025f61594f6a20722
julac <julace3@gmail.com>
2023-11-30 15:18:41 +0100

    changes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import base64
+from io import BytesIO
+
 from PIL import Image
 from flask import Flask, request
 import cv2
@@ -15,16 +18,15 @@ def analyze():
     content_type = request.headers.get("Content-Type")
     print(content_type)
     data = request.json
-    print(data.get("image"))
-    # image = request.files["image"] #odczytywanie obrazka z requesta
-    # img = Image.open(image)
-    # print(img.size)
-    # img = img.convert("L") #grayscale
-    # laplacian = cv2.Laplacian(np.asarray(img), cv2.CV_64F, ksize=3)
+    image_str = data.get("image")
+    img = Image.open(BytesIO(base64.b64decode(image_str)))
+    print(img.size)
+    img = img.convert("L") #grayscale
+    laplacian = cv2.Laplacian(np.asarray(img), cv2.CV_64F, ksize=3)
     # Convert back to uint8 and normalize values to 0-255 range
     # laplacian = np.uint8(np.absolute(laplacian))
     # laplacian = cv2.normalize(laplacian, None, 0, 255, cv2.NORM_MINMAX)
-    # return str(np.amax(laplacian))
+    return str(np.amax(laplacian))
 
 if __name__ == '__main__':
     app.run()
